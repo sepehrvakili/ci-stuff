0193e8bbb7cf98b53477e088e282586063d93af6
jduv <jduv@7factor.io>
2017-10-18 23:47:29 -0400

    Adding back build scripts

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# This script will build the entire project using standardized
+# GoLang tooling. Make sure you've sourced the gopath first or
+# this will not worky.
+go fmt 7factor.io/...
+go install 7factor.io/...
+go test --short --cover 7factor.io/...
+
+echo "Generating API documentation..."
+if command -v aglio >/dev/null; then
+    aglio -i texter.apib -o docs/api.html
+else
+    echo "Cannot generate documentation!"
+    echo "Program aglio not found. Please install by running:"
+    echo "npm -g install aglio"
+fi
---
+++ integration_tests.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Rebuild our containers
+docker-compose build
+
+# This is the least invasive way to execute our integration tests. We do not want to
+# put anything in the container that smells like test scripts.
+docker-compose run -w "/go" --entrypoint "/bin/bash -c \"/go/bin/server & go test --cover -run .*Integration 7factor.io/texter/...\"" texter
